6e0cbe8f41a9f7ec561e35d21ecaeb5570ac80a3
TADOKORO Saneyuki <saneyan@gfunction.com>
2017-11-25 07:34:52 +0900

    remove right side menu

--- src/app.vue
+++ src/app.vue
@@ -15,36 +15,6 @@
             <f7-block inner>
               <p>Left panel content goes here</p>
             </f7-block>
-            <f7-block-title>Load page in panel</f7-block-title>
-            <f7-list>
-              <f7-list-item link="/about/" title="About"></f7-list-item>
-              <f7-list-item link="/form/" title="Form"></f7-list-item>
-            </f7-list>
-            <f7-block-title>Load page in main view</f7-block-title>
-            <f7-list>
-              <f7-list-item link="/about/" title="About" link-view="#main-view" link-close-panel></f7-list-item>
-              <f7-list-item link="/form/" title="Form" link-view="#main-view" link-close-panel></f7-list-item>
-            </f7-list>
-          </f7-page>
-        </f7-pages>
-      </f7-view>
-    </f7-panel>
-
-    <!-- Right Panel -->
-    <f7-panel right cover layout="dark">
-      <f7-view id="right-panel-view" navbar-through :dynamic-navbar="true">
-        <f7-navbar v-if="$theme.ios" title="Right Panel" sliding></f7-navbar>
-        <f7-pages>
-          <f7-page>
-            <f7-navbar v-if="$theme.material" title="Right Panel" sliding></f7-navbar>
-            <f7-block>
-              <p>Right panel content goes here</p>
-            </f7-block>
-            <f7-block-title>Load page in panel</f7-block-title>
-            <f7-list>
-              <f7-list-item link="/about/" title="About"></f7-list-item>
-              <f7-list-item link="/form/" title="Form"></f7-list-item>
-            </f7-list>
             <f7-block-title>Load page in main view</f7-block-title>
             <f7-list>
               <f7-list-item link="/about/" title="About" link-view="#main-view" link-close-panel></f7-list-item>
@@ -64,9 +34,6 @@
             <f7-link icon="icon-bars" open-panel="left"></f7-link>
           </f7-nav-left>
           <f7-nav-center sliding>Framework7</f7-nav-center>
-          <f7-nav-right>
-            <f7-link icon="icon-bars" open-panel="right"></f7-link>
-          </f7-nav-right>
         </f7-navbar>
         <!-- Pages -->
         <f7-pages>
@@ -77,9 +44,6 @@
                 <f7-link icon="icon-bars" open-panel="left"></f7-link>
               </f7-nav-left>
               <f7-nav-center sliding>Framework7</f7-nav-center>
-              <f7-nav-right>
-                <f7-link icon="icon-bars" open-panel="right"></f7-link>
-              </f7-nav-right>
             </f7-navbar>
             <!-- Page Content -->
 
